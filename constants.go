983746b1e19df85eb1fafd00e9a4a26b2a1de74f
Philippe DeSousa <phil.desousa@gmail.com>
2026-02-24 21:14:18 -0500

    initial commit

---
+++ constants.go
@@ -0,0 +1,10 @@
+package main
+
+// const OriginURL = "http://localhost:5173"
+const OriginURL = "https://d3knrnqllqxltd.cloudfront.net"
+const TableName = "MyrestoImages"
+const ApiPath = "images"
+
+// const localMode = true
+
+const localMode = false
