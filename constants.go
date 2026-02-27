3ae8e1007af617aead39ec6aee56d77229aa657a
Philippe DeSousa <phil.desousa@gmail.com>
2026-02-27 15:40:15 -0500

    revert localMode

--- constants.go
+++ constants.go
@@ -1,12 +1,12 @@
 package main
 
-const OriginURL = "http://localhost:5173"
+// const OriginURL = "http://localhost:5173"
 
-// const OriginURL = "https://d3knrnqllqxltd.cloudfront.net"
+const OriginURL = "https://d3knrnqllqxltd.cloudfront.net"
 const ApiPath = "images"
 
-const localMode = true
+// const localMode = true
 
-// const localMode = false
+const localMode = false
 
 const bucketName = "myrestowebsite-4b5e107b"
